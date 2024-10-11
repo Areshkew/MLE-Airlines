dba32a570d2344a7e45558eb1a02d1e0f221c1ab
Gustavo Ramirez <jungdrew@yahoo.com>
2024-10-11 18:37:46 -0500

    Add API, documentation.

--- challenge/api.py
+++ challenge/api.py
@@ -1,6 +1,50 @@
 import fastapi
+import pandas as pd
+from pydantic import BaseModel
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from challenge.model import DelayModel
+from fastapi import HTTPException
 
 app = fastapi.FastAPI()
+model = None 
+valid_values = {}
+
+class Flight(BaseModel):
+    OPERA: str
+    TIPOVUELO: str
+    MES: int
+
+class PredictionInput(BaseModel):
+    flights: list[Flight]
+    
+@app.on_event("startup")
+def load_model():
+    global model
+    
+    # 
+    data_path = "data/data.csv"
+    data = pd.read_csv(data_path)
+    
+    # Store the unique values for OPERA, TIPOVUELO, and MES
+    valid_values['OPERA'] = set(data['OPERA'].unique())
+    valid_values['TIPOVUELO'] = set(data['TIPOVUELO'].unique())
+    valid_values['MES'] = set(data['MES'].unique())
+    
+    #
+    model = DelayModel()
+
+    #
+    features, target = model.preprocess(
+        data = data,
+        target_column="delay"
+    )
+
+    model.fit(
+        features=features,
+        target=target
+    )
+
 
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
@@ -9,5 +53,33 @@ async def get_health() -> dict:
     }
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(input_data: PredictionInput) -> dict:
+    global model
+
+    if model is None:
+        raise HTTPException(status_code=400, detail="Model is not initialized.")
+
+    # 
+    input_df = pd.DataFrame([flight.dict(by_alias=True) for flight in input_data.flights])
+
+    for _, row in input_df.iterrows():
+        if row['OPERA'] not in valid_values['OPERA']:
+            raise HTTPException(status_code=400, detail=f"OPERA value '{row['OPERA']}' is not valid.")
+        if row['TIPOVUELO'] not in valid_values['TIPOVUELO']:
+            raise HTTPException(status_code=400, detail=f"TIPOVUELO value '{row['TIPOVUELO']}' is not valid.")
+        if row['MES'] not in valid_values['MES']:
+            raise HTTPException(status_code=400, detail=f"MES value '{row['MES']}' is not valid.")
+
+    # 
+    try:
+        processed_input = model.preprocess(input_df)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Data preprocessing error: {e}")
+
+    #
+    try:
+        prediction = model.predict(processed_input)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Prediction error: {e}")
+
+    return {"predict": prediction}
--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,67 +1,73 @@
 import unittest
+from unittest.mock import ANY
 
 from fastapi.testclient import TestClient
-from challenge import app
-
+from mockito import when
+import numpy as np
+from challenge.api import app
 
 class TestBatchPipeline(unittest.TestCase):
     def setUp(self):
         self.client = TestClient(app)
         
     def test_should_get_predict(self):
-        data = {
-            "flights": [
-                {
-                    "OPERA": "Aerolineas Argentinas", 
-                    "TIPOVUELO": "N", 
-                    "MES": 3
-                }
-            ]
-        }
-        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0])) # change this line to the model of chosing
-        response = self.client.post("/predict", json=data)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {"predict": [0]})
+        with self.client as client:
+            data = {
+                "flights": [
+                    {
+                        "OPERA": "Aerolineas Argentinas", 
+                        "TIPOVUELO": "N", 
+                        "MES": 3
+                    }
+                ]
+            }
+            when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0])) # change this line to the model of chosing
+            response = client.post("/predict", json=data)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json(), {"predict": [0]})
     
 
     def test_should_failed_unkown_column_1(self):
-        data = {       
-            "flights": [
-                {
-                    "OPERA": "Aerolineas Argentinas", 
-                    "TIPOVUELO": "N",
-                    "MES": 13
-                }
-            ]
-        }
-        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))# change this line to the model of chosing
-        response = self.client.post("/predict", json=data)
-        self.assertEqual(response.status_code, 400)
+        with self.client as client:
+            data = {       
+                "flights": [
+                    {
+                        "OPERA": "Aerolineas Argentinas", 
+                        "TIPOVUELO": "N",
+                        "MES": 13
+                    }
+                ]
+            }
+            when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))# change this line to the model of chosing
+            response = client.post("/predict", json=data)
+            self.assertEqual(response.status_code, 400)
 
     def test_should_failed_unkown_column_2(self):
-        data = {        
-            "flights": [
-                {
-                    "OPERA": "Aerolineas Argentinas", 
-                    "TIPOVUELO": "O", 
-                    "MES": 13
-                }
-            ]
-        }
-        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))# change this line to the model of chosing
-        response = self.client.post("/predict", json=data)
-        self.assertEqual(response.status_code, 400)
+        with self.client as client:
+            data = {        
+                "flights": [
+                    {
+                        "OPERA": "Aerolineas Argentinas", 
+                        "TIPOVUELO": "O", 
+                        "MES": 13
+                    }
+                ]
+            }
+            when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))# change this line to the model of chosing
+            response = client.post("/predict", json=data)
+            self.assertEqual(response.status_code, 400)
     
     def test_should_failed_unkown_column_3(self):
-        data = {        
-            "flights": [
-                {
-                    "OPERA": "Argentinas", 
-                    "TIPOVUELO": "O", 
-                    "MES": 13
-                }
-            ]
-        }
-        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))
-        response = self.client.post("/predict", json=data)
-        self.assertEqual(response.status_code, 400)
+        with self.client as client:
+            data = {        
+                "flights": [
+                    {
+                        "OPERA": "Argentinas", 
+                        "TIPOVUELO": "O", 
+                        "MES": 13
+                    }
+                ]
+            }
+            when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))
+            response = client.post("/predict", json=data)
+            self.assertEqual(response.status_code, 400)
