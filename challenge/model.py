69cb93025a4c2e6542d06a49d441082040f0aebf
Gustavo Ramirez <jungdrew@yahoo.com>
2024-10-11 09:56:37 -0500

    Add data preprocessing for the model

--- challenge/model.py
+++ challenge/model.py
@@ -1,6 +1,11 @@
 import pandas as pd
-
+import numpy as np
 from typing import Tuple, Union, List
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from sklearn.metrics import confusion_matrix, classification_report
+
+from challenge.preprocess_utils import *
 
 class DelayModel:
 
@@ -13,7 +18,7 @@ class DelayModel:
         self,
         data: pd.DataFrame,
         target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
         """
         Prepare raw data for training or predict.
 
@@ -26,7 +31,23 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+        # Feature Generation
+        threshold_in_minutes = 15
+        data['period_day'] = data['Fecha-I'].apply(get_period_day)
+        data['high_season'] = data['Fecha-I'].apply(is_high_season)
+        data['min_diff'] = data.apply(get_min_diff, axis = 1)
+        data['delay'] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)
+        
+        if target_column: # If a target column is provided, return both features and target
+            if target_column in data.columns: # Ensure the target column exists
+                target = data[target_column]
+                features = data.drop(target_column, axis=1)
+                return features, target
+            else:
+                raise ValueError(f"Target column '{target_column}' not found in data.")
+        
+        # Return only features
+        return data
 
     def fit(
         self,
---
+++ docs/challenge.md
@@ -0,0 +1,4 @@
+<!-- Documentation and Explanations -->
+
+# Part I
+- 
